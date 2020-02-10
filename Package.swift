dfa275f7360a971ae37132e0f8232a0ae40119a0
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-10 16:06:09 +0100

    temp fix

--- Package.swift
+++ Package.swift
@@ -3,21 +3,23 @@
 
 import PackageDescription
 
+// TODO: resolve problem with sources shared to all targets
+
 let package = Package(
     name: "KeyedCodable",
-    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
+//    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.iOS(.v8)],
     products: [
-        .library(name: "KeyedCodable",
-                 targets: ["KeyedCodable-iOS",
-                    "KeyedCodable-watchOS",
-                    "KeyedCodable-tvOS",
-                    "KeyedCodable-macOS"]
+        .library(
+            name: "KeyedCodable",
+//            targets: ["KeyedCodable-iOS", "KeyedCodable-watchOS", "KeyedCodable-tvOS", "KeyedCodable-macOS"]
+            targets: ["KeyedCodable-iOS"]
         ),
     ],
     targets: [
         .target(name: "KeyedCodable-iOS", path: "KeyedCodable/Sources"),
-        .target(name: "KeyedCodable-watchOS", path: "KeyedCodable/Sources"),
-        .target(name: "KeyedCodable-tvOS", path: "KeyedCodable/Sources"),
-        .target(name: "KeyedCodable-macOS", path: "KeyedCodable/Sources")
+//        .target(name: "KeyedCodable-watchOS", path: "KeyedCodable/Sources"),
+//        .target(name: "KeyedCodable-tvOS", path: "KeyedCodable/Sources"),
+//        .target(name: "KeyedCodable-macOS", path: "KeyedCodable/Sources")
     ]
 )
