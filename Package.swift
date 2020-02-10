334d5c22dfa66d1a51b1205ec39c9ac6d202ecfb
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-10 15:40:08 +0100

    Add SPM manifest file.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KeyedCodable",
+    platforms: [.macOS(.v10_9), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(name: "KeyedCodable",
+                 targets: ["KeyedCodable-iOS",
+                    "KeyedCodable-watchOS",
+                    "KeyedCodable-tvOS",
+                    "KeyedCodable-macOS"]
+        ),
+    ],
+    targets: [
+        .target(name: "KeyedCodable-iOS", path: "Sources"),
+        .target(name: "KeyedCodable-watchOS", path: "Sources"),
+        .target(name: "KeyedCodable-tvOS", path: "Sources"),
+        .target(name: "KeyedCodable-macOS", path: "Sources")
+    ]
+)
